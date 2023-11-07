c09af110b36f533926ef14daaa2319bd5edcdd0b
jsiya <jabbarovasia@gmail.com>
2023-11-08 00:00:08 +0400

    Update CreateRideViewModel

--- SchoolBusProject/ViewModels/CreateRideViewModel.cs
+++ SchoolBusProject/ViewModels/CreateRideViewModel.cs
@@ -1,13 +1,43 @@
 ﻿using GalaSoft.MvvmLight;
+using SchoolBusDataAccess.Repositories.Abstracts;
+using SchoolBusDataAccess.Repositories.Concretes;
+using SchoolBusModels.Concretes;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SchoolBusProject.ViewModels
+namespace SchoolBusProject.ViewModels;
+
+public class CreateRideViewModel : ViewModelBase, INotifyPropertyChanged
 {
-    public class CreateRideViewModel : ViewModelBase
+
+    private Ride _selectedItem;
+
+    public Ride SelectedItem
     {
+        get { return _selectedItem; }
+        set { _selectedItem = value; OnPropertyChanged(); }
+    }
+    public ObservableCollection<Student> Students { get; set; }
+    public ObservableCollection<Student> SelectedStudents { get; set; } = new ObservableCollection<Student>();
+    public IRepository<Ride> RidesRepo { get; set; }
+    public IRepository<Student> StudentsRepo { get; set; }
+
+    public CreateRideViewModel()
+    {
+        RidesRepo = new Repository<Ride>();
+        StudentsRepo = new Repository<Student>();
+        Students = new ObservableCollection<Student>(StudentsRepo.GetAll());
+    }
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void OnPropertyChanged([CallerMemberName] string? name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
