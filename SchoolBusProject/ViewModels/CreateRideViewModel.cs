0b81c50cfc353e66c4866cb170e8fcda22cca897
jsiya <jabbarovasia@gmail.com>
2023-11-14 22:10:33 +0400

    update CreateRideView

--- SchoolBusProject/ViewModels/CreateRideViewModel.cs
+++ SchoolBusProject/ViewModels/CreateRideViewModel.cs
@@ -18,15 +18,48 @@ namespace SchoolBusProject.ViewModels;
 public class CreateRideViewModel : ViewModelBase, INotifyPropertyChanged
 {
 
-    private Ride _selectedItem;
+    private Student _selectedItem;
 
-    public Ride SelectedItem
+    public Student SelectedItem
     {
         get { return _selectedItem; }
         set { _selectedItem = value; OnPropertyChanged(); }
     }
 
+    private Car _selectedCar;
+
+    public Car SelectedCar
+    {
+        get { return _selectedCar; }
+        set { _selectedCar = value; OnPropertyChanged(); }
+    }
+
+    private Driver _selectedDriver;
+
+    public Driver SelectedDriver
+    {
+        get { return _selectedDriver; }
+        set { _selectedDriver = value; OnPropertyChanged(); }
+    }
+
+    private DateTime _selectedDate;
+
+    public DateTime SelectedDate
+    {
+        get { return _selectedDate; }
+        set { _selectedDate = value; OnPropertyChanged(); }
+    }
+
+    private Ride _ride;
+
+    public Ride Ridee
+    {
+        get { return _ride; }
+        set { _ride = value; OnPropertyChanged(); }
+    }
+
     private string _count;
+    private int _counter = 0;
 
     public string Count
     {
@@ -35,26 +68,56 @@ public class CreateRideViewModel : ViewModelBase, INotifyPropertyChanged
     }
     public ICommand? AddStudent { get; set; }
     public ICommand? RemoveStudent { get; set; }
+    public ICommand? CreateRide { get; set; }
     public ObservableCollection<Student> Students { get; set; }
-    public ObservableCollection<Student> SelectedStudents { get; set; } = new ObservableCollection<Student>();
+    public ObservableCollection<Driver> Drivers { get; set; }
+    public ObservableCollection<Car> Cars { get; set; }
     public IRepository<Ride> RidesRepo { get; set; }
     public IRepository<Student> StudentsRepo { get; set; }
+    public IRepository<Driver> DriverRepo { get; set; }
+    public IRepository<Car> CarRepo { get; set; }
 
     public CreateRideViewModel()
     {
+        Ridee = new Ride();
         RidesRepo = new Repository<Ride>();
         StudentsRepo = new Repository<Student>();
+        DriverRepo = new Repository<Driver>();
+        CarRepo = new Repository<Car>();
         Students = new ObservableCollection<Student>(StudentsRepo.GetAll());
+        Drivers = new ObservableCollection<Driver>(DriverRepo.GetAll());
+        Cars = new ObservableCollection<Car>(CarRepo.GetAll());
         AddStudent = new RelayCommand(AddStudentMethod);
+        RemoveStudent = new RelayCommand(RemoveStudentMethod);
     }
 
     private void AddStudentMethod()
     {
+        Ridee.StudentRides.Add(new StudentRide() { Student = SelectedItem});
+        Students.Remove(SelectedItem);
 
+        _counter++;
+        Count = _counter.ToString();
     }
     private void RemoveStudentMethod()
     {
+        if(Ridee.StudentRides.Count > 0)
+        {
+            Ridee.StudentRides.Remove(Ridee.StudentRides.FirstOrDefault(s=>s.Student == SelectedItem));
 
+            _counter--;
+            Count = _counter.ToString();
+
+            Students.Add(SelectedItem);
+        }
+    }
+
+    private void CreateRideMethod()
+    {
+        Ridee.Car = SelectedCar;
+        Ridee.Driver = SelectedDriver;
+        Ridee.StartTime = SelectedDate;
+        Ridee.EndTime = SelectedDate.AddDays(1);
     }
 
     public event PropertyChangedEventHandler? PropertyChanged;
